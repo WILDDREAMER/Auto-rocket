40a97778d79d3733ab66851e3d5beac35217aaac
WILDDREAMER <oussama.zakkare@gmail.com>
2021-03-25 02:18:31 +0100

    adding-h2

--- src/components/main/footer.js
+++ src/components/main/footer.js
@@ -2,6 +2,7 @@ export default function Footer(params) {
     return (
         <footer>
             <h1>Share this list!</h1>
+            <h2></h2>
             <p className='infos'>Need another opinion?<br/>Send it to another number</p>
         </footer>
     )

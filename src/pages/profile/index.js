f4c4abb11115894a080da669957c8c97bedfd988
OUSSAMA ZAKKARE <53281129+WILDDREAMER@users.noreply.github.com>
2021-04-14 13:21:32 +0000

    profile

---
+++ src/pages/profile/index.js
@@ -0,0 +1,8 @@
+export default function Profile(props) {
+    <div className='profile'>
+        <h1>Profile</h1>
+        <img src="" alt="" />
+        <p className='number'></p>
+        <p className='addName'></p>
+    </div>
+}
